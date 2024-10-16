bf9eda1676feb49634cefa47ddc905df2e3f18b4
MattyHungry <140957@edu.pcc.nu>
2024-10-16 07:56:15 +0000

    verandering
    Co-authored-by: pcc-140716 <pcc-140716@users.noreply.github.com>

--- Opdracht.py
+++ Opdracht.py
@@ -1,14 +1,12 @@
 import re 
 import random
 
-# Dictionary voor patronen.
 patterns = { 
    "Weet je nog (.*)":"Natuurlijk weet nog {}", 
-   "Ik voel (.*)":"Waarom voel jij je {}?" 
+   "Ik voel me (.*)":"Waarom voel jij je {}?" 
 
 }
 
-# Dictionary voor veelvoorkomende berichten.
 responses = {
   "Hallo": ["Hoi, hoe gaat het?", "Hallo!"],
   "Het gaat goed": ["Goed om te horen!", "Geweldig!"]
@@ -16,11 +14,12 @@ responses = {
 }
 
 def get_response(message):
+
+
   for pattern in patterns: 
-     match = re.search(pattern,message) 
-     if match:
-        print("Match found!") 
-        print("USER: " + match.group(0))
+    match = re.search(pattern, message) 
+    if match:
+      return patterns[pattern].format(match.group(1))
 
   if message in responses: 
     return random.choice(responses[message]) 
