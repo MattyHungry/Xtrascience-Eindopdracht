4555b6772284ad6fec2e81989ef71bdaffd7e842
MattyHungry <140957@edu.pcc.nu>
2024-10-23 07:35:28 +0000

    23 Oktober
    Co-authored-by: pcc-140716 <pcc-140716@users.noreply.github.com>

--- Opdracht.py
+++ Opdracht.py
@@ -1,15 +1,40 @@
 import re 
 import random
+import json
+import urllib.request
 
 patterns = { 
-   "Weet je nog (.*)":"Natuurlijk weet nog {}", 
-   "Ik voel me (.*)":"Waarom voel jij je {}?" 
+   "Weet je nog (.*)":"Natuurlijk weet ik nog {}.",
+   "weet je nog (.*)":"Natuurlijk weet ik nog {}.",    
+   "Ik voel me (.*)":"Waarom voel jij je {}?",
+   "ik voel me (.*)":"Waarom voel jij je {}?",
+   "Ik wil graag (.*)": "Ik snap dat jij graag wilt{}.",
+   "ik wil graag (.*)": "Ik snap dat jij graag wilt{}.",
+   "Hoe lang duurt het nog totdat mijn pakketje komt (.*)": "Het duur nog ongeveer 5 dagen{}.",
+   "hoe lang duurt het nog totdat mijn pakketje komt (.*)": "Ik snap dat jij graag wilt{}."
 
 }
 
+def get_product_fact():
+  url = 'https://fakestoreapi.com/products/1'
+  response = urllib.request.urlopen(url)
+  result = json.loads(response.read())
+  
+  single_products = result["data"]
+  single_product = single_products[0]
+  
+  return single_products
+
+def get_response(message): 
+  if message in responses: 
+      return "Bot: " + responses[message] 
+  else: 
+      return "I heard you. You said: " + message 
+  
 responses = {
   "Hallo": ["Hoi, hoe gaat het?", "Hallo!"],
-  "Het gaat goed": ["Goed om te horen!", "Geweldig!"]
+  "Het gaat goed": ["Goed om te horen!", "Geweldig!"],
+  "product" : get_product_fact()
 
 }
 
