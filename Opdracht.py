0816f76ed50fe18b9ce5163ab07798ecff22fda5
MattyHungry <140957@edu.pcc.nu>
2024-11-06 09:04:42 +0000

    Co-authored-by: pcc-140716 <pcc-140716@users.noreply.github.com>

--- Opdracht.py
+++ Opdracht.py
@@ -1,3 +1,4 @@
+#werkt alleen in trinket
 import re 
 import random
 import json
@@ -10,17 +11,14 @@ patterns = {
    "ik voel me (.*)":"Waarom voel jij je {}?",
    "Ik wil graag (.*)": "Ik snap dat jij graag wilt{}.",
    "ik wil graag (.*)": "Ik snap dat jij graag wilt{}.",
-   "Hoe lang duurt het nog totdat mijn pakketje komt (.*)": "Het duur nog ongeveer 5 dagen{}.",
-   "hoe lang duurt het nog totdat mijn pakketje komt (.*)": "Ik snap dat jij graag wilt{}."
-
-}
+    }
 
 def get_product_fact():
   url = 'https://fakestoreapi.com/products/1'
   response = urllib.request.urlopen(url)
   result = json.loads(response.read())
   
-  single_products = result["data"]
+  single_products = result["title"]
   single_product = single_products[0]
   
   return single_products
@@ -33,10 +31,20 @@ def get_response(message):
   
 responses = {
   "Hallo": ["Hoi, hoe gaat het?", "Hallo!"],
+  "hallo": ["Hoi, hoe gaat het?", "Hallo!"],
   "Het gaat goed": ["Goed om te horen!", "Geweldig!"],
-  "product" : get_product_fact()
+  "het gaat goed": ["Goed om te horen!", "Geweldig!"],
+  "product" : [get_product_fact()],
+  "Hoe lang duurt het nog totdat mijn pakketje komt": ["Het duurt nog ongeveer 5 dagen."],
+  "hoe lang duurt het nog totdat mijn pakketje komt": ["Het duurt nog ongeveer 5 dagen."],
+  "Wat zijn de kosten om mijn pakketje terug te sturen": ["Dat kost ongeveer €1.", "Dat kost ongeveer €2.", "Dat kost ongeveer €3.", "Dat kost ongeveer €4."],
+  "wat zijn de kosten om mijn pakketje terug te sturen": ["Dat kost tussen de €5-10."],
+  "Hoe komt het dat mijn pakketje vertraagt is": ["Dat komt omdat we het op dit moment druk hebben met veel bestellingen.", "Sorry, we hebben het op dit moment heel druk."],
+  "hoe komt het dat mijn pakketje vertraagt is": ["Dat komt omdat we het op dit moment druk hebben met veel bestellingen.", "Sorry, we hebben het op dit moment heel druk."],
+  "Is er een mogelijkheid dat ik mijn pakketje kan volgen": ["Met onze app kan je je pakketje volgen met de unieke code die je hebt ontvangen in je mail".],
+  "is er een mogelijkheid dat ik mijn pakketje kan volgen": ["Met onze app kan je je pakketje volgen met de unieke code die je hebt ontvangen in je mail".],
 
-}
+,}
 
 def get_response(message):
 
